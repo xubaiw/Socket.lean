86b96cf78fc3a76c6862a9306070fbba9787acf1
Xubai Wang <self@xubai.wang>
2022-01-24 17:42:09 +0800

    style: remove copyright headers

--- native/native.c
+++ native/native.c
@@ -1,9 +1,3 @@
-/*
-    Copyright (c) 2021 Xubai Wang. All rights reserved.
-    Released under Apache 2.0 license as described in the file LICENSE.
-    Authors: Xubai Wang
-*/
-
 // ==============================================================================
 // # Includes
 // ==============================================================================
