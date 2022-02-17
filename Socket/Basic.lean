6265ece92c744d8c210451037cae3bd2f389bea8
Xubai Wang <self@xubai.wang>
2022-02-18 00:46:49 +0800

    doc: update to new doc-gen link syntax

--- Socket/Basic.lean
+++ Socket/Basic.lean
@@ -4,9 +4,9 @@
 
   This module defines some basic types to be used across whole pacage:
 
-  - [`Socket`](#Socket.Socket): Opaque reference to underlying socket
-  - [`SockAddr`](#Socket.SockAddr): Opaque reference to underlying socket address
-  - [`AddressFamily`](#Socket.AddressFamily): Enumeration of supported address families
+  - `Socket`: Opaque reference to underlying socket
+  - `SockAddr`: Opaque reference to underlying socket address
+  - `AddressFamily`: Enumeration of supported address families
 -/
 
 namespace Socket
@@ -16,33 +16,31 @@ namespace Socket
 -/
 
 /--
-  Use `NonemptyType` to implement `Inhabited` for [`Socket`](#Socket.Socket).
+  Use `NonemptyType` to implement `Inhabited` for `Socket`.
   [detailed explanation](#explanation-usage-of-nonemptytype)
 -/
 constant Socket.Nonempty : NonemptyType
 
 /--
-  Opaque reference to underlying socket.
+  Opaque reference to underlying platform specific socket.
 
-  To create a `Socket`, refer to [`Socket.mk`](/find/Socket.Socket.mk).
-  Then you can manipulate the `Socket` using common socket functions like
-  [`bind`](/find/Socket.Socket.bind),
-  [`connect`](/find/Socket.Socket.connect), etc. For all functions
-  available, refer to the [`Socket`](/Socket.Socket) module.
+  To create a `Socket`, refer to [`Socket.mk`](#Socket.Socket.mk). Then you
+  can manipulate the `Socket` using common socket functions like
+  [`Socket.bind`](#Socket.Socket.bind),[`Socket.connect`](#Socket.Socket.connect),
+  etc. For all functions available, refer to the [`Socket` module](Socket/Socket.html).
 
-  *NOTE*: The `Socket` type is made opaque since implementation of socket may
-  be different across platforms (`int` on *nix and `SOCKET` on Windows).
+  *NOTE*: `NonemptyType` is used to implement `Inhabited` for `Socket`. [detailed explanation](#explanation-usage-of-nonemptytype)
 -/
 def Socket : Type := Socket.Nonempty.type
 
 /--
-  Use `NonemptyType` to implement `Inhabited` for [`Socket`](#Socket.Socket).
+  Use `NonemptyType` to implement `Inhabited` for `Socket`.
   [detailed explanation](#explanation-usage-of-nonemptytype)
 -/
 instance : Nonempty Socket := Socket.Nonempty.property
 
 /--
-  Use `NonemptyType` to implement `Inhabited` for [`SockAddr`](#Socket.SockAddr).
+  Use `NonemptyType` to implement `Inhabited` for `SockAddr`.
   [detailed explanation](#explanation-usage-of-nonemptytype)
 -/
 constant SockAddr.Nonempty : NonemptyType
@@ -55,7 +53,7 @@ constant SockAddr.Nonempty : NonemptyType
 def SockAddr : Type := SockAddr.Nonempty.type
 
 /--
-  Use `NonemptyType` to implement `Inhabited` for [`SockAddr`](#Socket.SockAddr)
+  Use `NonemptyType` to implement `Inhabited` for `SockAddr`
   [detailed explanation](#explanation-usage-of-nonemptytype)
 -/
 instance : Nonempty SockAddr := SockAddr.Nonempty.property
@@ -95,8 +93,8 @@ instance : ToString SockType where
     | SockType.dgram => "SOCK_DGRAM"
 
 /--
-  Enumeration of supported socket types,
-  which is used in [`Socket.shutdown`](/find/Socket.Socket.shutdown).
+  Enumeration of how is socket shutdown,
+  which is used in [`Socket.shutdown`](#Socket.Socket.shutdown).
 -/
 inductive ShutdownHow where
   | read
@@ -112,9 +110,8 @@ end Socket
 /-!
   ## Explanation: Usage of `NonemptyType`
 
-  As [`Socket`](#Socket.Socket) and [`SockAddr`](#Socket.SockAddr)
-  are implemented using `lean_external_class`. `NonemptyType` is used
-  to implement `Inhabited` for these types.
+  As `Socket` and `SockAddr` are implemented using`lean_external_class`.
+  `NonemptyType` is used to implement `Inhabited` for these types.
 
   A simple example of this trick can be found
   [here](https://github.com/xubaiw/lean4/blob/c1fccf19cb8f15daba2329e5c82c6feca825c02b/tests/compiler/foreign)
