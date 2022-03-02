752eadc681a3a4f967d4263c057fe41b851c9d2d
Xubai Wang <self@xubai.wang>
2022-03-02 23:10:11 +0800

    doc: use repeat for http-server

--- examples/http-server/Main.lean
+++ examples/http-server/Main.lean
@@ -3,23 +3,6 @@ import Socket
 open Socket
 
 /--
-  Send back hello on accept.
--/
-partial def serve (socket : @& Socket) : IO Unit := do
-  let (remoteAddr, socket') ← socket.accept
-  let t ← IO.asTask do
-    let strSend := 
-      "HTTP/1.1 200 OK" ++
-      "Content-Length:5" ++
-      "\r\n\r\n" ++
-      "Hello" ++
-      "\r\n\r\n"
-    let bytesSend ← socket'.send strSend.toUTF8
-    socket'.close
-  IO.println s!"Incoming: {remoteAddr}"
-  serve socket
-
-/--
   Entry
 -/
 def main : IO Unit := do
@@ -37,4 +20,15 @@ def main : IO Unit := do
   IO.println s!"Listening at http://localhost:8080."
 
   -- serving
-  serve socket
+  repeat do
+    let (remoteAddr, socket') ← socket.accept
+    let t ← IO.asTask do
+      let strSend := 
+        "HTTP/1.1 200 OK" ++
+        "Content-Length:5" ++
+        "\r\n\r\n" ++
+        "Hello" ++
+        "\r\n\r\n"
+      let bytesSend ← socket'.send strSend.toUTF8
+      socket'.close
+    IO.println s!"Incoming: {remoteAddr}"
